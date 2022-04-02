d03e3b73e130979a8c3518a190a230eded8d8455
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-04-02 17:05:35 +0200

    seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from  '@prisma/client';
+import faker from 'faker';
+
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      email: `testemail+1@gmail.com`,
+      role: 'ADMIN',
+    },
+  })
+
+  const question = await prisma.question.create({
+    data: {
+      question: 'What\'s that name',
+      correctAnswer: 'adam'
+    }
+  })
+
+  await prisma.wrongAnswer.create({
+    data: {
+      answer: 'John',
+      questionId: question.id
+    }
+  })
+}
+
+main()
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
